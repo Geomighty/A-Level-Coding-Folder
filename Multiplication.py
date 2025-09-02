d399fedbf72c0fa32d7a3c1b74ac0f1c4f0cb07a
Geomighty <george18bkh@gmail.com>
2025-09-02 14:27:06 +0100

    Multiplication Project Starter Task

---
+++ Multiplication.py
@@ -0,0 +1,8 @@
+multiplication_table = int(input("enter the number of the multiplication table you would like"))
+multiples = int(input("enter the number of multiples of that number you would like to know"))
+results = []
+for i in range(multiples):
+    multiplication_results = multiplication_table * (i+1)
+    results.append(multiplication_results)
+for j in range(multiples):
+    print(f"{multiplication_table} X {j + 13} = {results[j]}")

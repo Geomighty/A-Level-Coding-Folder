e40a1b158454d9e57ce2a255da4f1889c0287dca
Geomighty <george18bkh@gmail.com>
2025-10-11 12:12:08 +0100

    I can't work out what has gone wrong with this but it works to a degree.

--- OOP.py
+++ OOP.py
@@ -52,10 +52,36 @@ class Card:
 
     def get_short_name(self):
         """ return card name eg '10D' '8C' 'AH' """
+        value_list = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        suite_list = ["S", "H", "D", "C"]
+        value_list_index = self._card_number % 13
+        suite_list_index = [1, 2, 3, 4]
+        if self._card_number <= 12:
+            index = suite_list_index[0]
+        elif self._card_number >= 13 and self._card_number <= 25:
+            index = suite_list_index[1]
+        elif self._card_number >= 26 and self._card_number <= 38:
+            index = suite_list_index[2]
+        elif self._card_number >= 39 and self._card_number <= 51:
+            index = suite_list_index[3]
+        return f"{value_list[value_list_index]}{suite_list[suite_list_index[index]]}"
         pass
 
     def get_long_name(self):
         """ return card name eg 'Ten of Diamonds' """
+        value_list = ["Ace", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+        suite_list = ["Spades", "Hearts", "Diamonds", "Clubs"]
+        value_list_index = self._card_number % 13
+        suite_list_index = [1, 2, 3, 4]
+        if self._card_number <= 12:
+            index = suite_list_index[0]
+        elif self._card_number >= 13 and self._card_number <= 25:
+            index = suite_list_index[1]
+        elif self._card_number >= 26 and self._card_number <= 38:
+            index = suite_list_index[2]
+        elif self._card_number >= 39 and self._card_number <= 51:
+            index = suite_list_index[3]
+        return f"{value_list[value_list_index]} of {suite_list[suite_list_index[index]]}"
         pass
 
 
@@ -68,6 +94,7 @@ class Deck:
 
     def length(self):
         """ returns the number of cards still in the deck """
+        return len(self._card_list)
         pass
 
     def shuffle_deck(self):
@@ -76,6 +103,8 @@ class Deck:
 
     def take_top_card(self):
         """ removes the top card from the deck and returns it (as a card object) """
+        top_card = self._card_list[0]
+        return top_card
         pass
 
     def add_card(self, new_card):
@@ -90,3 +119,4 @@ deck.shuffle_deck()
 for _ in range(deck.length()):
     card = deck.take_top_card()
     print(card.get_long_name())
+    deck.shuffle_deck()
